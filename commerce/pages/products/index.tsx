7ba34035b389fa8ba37d0e6b1d7ac51627c461c0
dev_sm <tmdals9974@naver.com>
2023-05-20 16:40:58 +0900

    feat: 5-3-6강 학습내용 정리

--- commerce/pages/products/index.tsx
+++ commerce/pages/products/index.tsx
@@ -1,5 +1,6 @@
 import { products } from '@prisma/client';
-import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { useCallback, useEffect, useState } from 'react';
 
 const TAKE = 9;
 export default function Products() {
@@ -7,14 +8,54 @@ export default function Products() {
   const [products, setProducts] = useState<products[]>();
 
   useEffect(() => {
-    fetch(`/api/get-products&skip=0&take=${TAKE}`)
+    fetch(`/api/get-products?skip=0&take=${TAKE}`)
       .then((res) => res.json())
       .then((data) => setProducts(data.items));
   }, []);
 
+  const getProducts = useCallback(() => {
+    const next = skip + TAKE;
+    fetch(`/api/get-products?skip=0&skip=${next}&take=${TAKE}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const list = products?.concat(data.items);
+        setProducts(list);
+      });
+    setSkip(next);
+  }, [skip, products]);
+
   return (
-    <div>
-      {products && products.map((item) => <div key={item.id}>{item.name}</div>)}
+    <div className="px-36 mt-36 mb-36">
+      {products && (
+        <div className="grid grid-cols-3 gap-5">
+          {products.map((item) => (
+            <div key={item.id}>
+              <Image
+                className="rounded"
+                src={item.image_url ?? ''}
+                width={300}
+                height={200}
+                alt={item.name}
+              />
+              <div className="flex">
+                <span>{item.name}</span>
+                <span className="ml-auto">
+                  {item.price.toLocaleString('ko-KR')}원
+                </span>
+              </div>
+              <span className="text-zinc-400">
+                {item.category_id === 1 && '의류'}
+              </span>
+            </div>
+          ))}
+        </div>
+      )}
+      <button
+        className="w-full rounded mt-20 bg-zinc-200 p-4"
+        onClick={getProducts}
+      >
+        더보기
+      </button>
     </div>
   );
 }
